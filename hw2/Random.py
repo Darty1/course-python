20811fb5b54ac5afb493a28bb38af2da0d47a721
Darty <tatyana.matlasch@gmail.com>
2019-06-10 14:01:00 +0300

    gitignore

---
+++ hw2/Random.py
@@ -0,0 +1,13 @@
+import random as rand
+
+
+n = rand.randint(1, 10)
+
+k = input('guess, please: ')
+while k != n:
+    k = input('no, guess again, please: ')
+else:
+    print("you win, it's ", k)
+
+
+
